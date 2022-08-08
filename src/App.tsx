981f115ca7e0f0d805fa6532ab08d1c4763604d1
Saron Medeiros Philippi <saron.ph@gmail.com>
2022-08-08 14:30:38 -0300

    added context

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import { defaultTheme } from './styles/themes/default';
 
 import { Router } from './Router';
 import { BrowserRouter } from 'react-router-dom';
+import { CyclesContextProvider } from './contexts/CyclesContext';
 
 export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
         <GlobalStyle />
       </ThemeProvider>
     </BrowserRouter>
